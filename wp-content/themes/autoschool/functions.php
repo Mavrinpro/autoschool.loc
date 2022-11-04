13d7fc8dd4aa38b8bde8c3eccb1ca665bc7fd73f
keepe <mavrin79@mail.ru>
2022-11-04 23:52:38 +0400

    css hover arrow

--- wp-content/themes/autoschool/functions.php
+++ wp-content/themes/autoschool/functions.php
@@ -148,16 +148,18 @@ add_action( 'widgets_init', 'autoschool_widgets_init' );
 function autoschool_scripts() {
 	wp_enqueue_style( 'autoschool-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'autoschool-style', 'rtl', 'replace' );
-
+	if (is_page(82)) {
+		wp_enqueue_style( 'autoschool-fancybox-css', get_template_directory_uri() . '/css/fancybox.min.css', array(), _S_VERSION );
+	}
 	wp_enqueue_script( 'autoschool-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
-	if (is_page('photogallery')){
+	if (is_page(82)){
 		wp_enqueue_script( 'autoschool-fancybox', get_template_directory_uri() . '/js/fancybox.min.js', array(), _S_VERSION, true );
-		wp_enqueue_style('autoschool-fancybox-css', get_template_directory_uri() . '/css/fancybox.min.css', array(), _S_VERSION, true );
+
 	}
 }
 add_action( 'wp_enqueue_scripts', 'autoschool_scripts' );
--- wp-content/themes/autoschool/js/customizer.js
+++ wp-content/themes/autoschool/js/customizer.js
@@ -7,6 +7,8 @@
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
  */
 
+
+
 ( function( $ ) {
 	$('li.menu-item-has-children').addClass('nav-item dropdown');
 	$('.nav-item.dropdown').children('a.nav-link.waves-effect.waves-light').addClass('nav-link dropdown-toggle waves-effect waves-light').attr('data-toggle', 'dropdown');
